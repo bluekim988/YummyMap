a10141dcf5f109cd48f60b2ecfb94f7b41870ab5
aktmzk112 <aktmzk112@gmail.com>
2020-05-24 19:26:08 +0900

    recommit

--- YummyMap/src/com/yummymap/www/controller/admin/AdminEditProc.java
+++ YummyMap/src/com/yummymap/www/controller/admin/AdminEditProc.java
@@ -2,15 +2,56 @@ package com.yummymap.www.controller.admin;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import com.yummymap.www.DAO.AdminDAO;
 import com.yummymap.www.controller.MmyController;
+import com.yummymap.www.vo.admin.MemberInfoVO;
 
 public class AdminEditProc implements MmyController {
 
 	@Override
 	public String exec(HttpServletRequest req, HttpServletResponse resp) {
-		// TODO Auto-generated method stub
-		return null;
+		HttpSession session = req.getSession();
+		String sid = (String) session.getAttribute("SID");
+		String name = req.getParameter("name");
+		String pw = req.getParameter("pw");
+		String tel = req.getParameter("tel");
+		String mailid = req.getParameter("mail");
+		String domain = req.getParameter("domain");
+		String mail = mailid + "@" + domain;
+		
+		
+		
+	 if(pw.length() != 0) {
+		MemberInfoVO mvo = new MemberInfoVO();
+		mvo.setMid(sid);
+		mvo.setMname(name);
+		mvo.setPass(pw);
+		mvo.setMtel(tel);
+		mvo.setMemail(mail);
+
+		
+		AdminDAO adao = new AdminDAO();
+		adao.reAdmin(mvo);
+
+	 }else {
+			MemberInfoVO mvo = new MemberInfoVO();
+			mvo.setMid(sid);
+			mvo.setMname(name);
+			mvo.setMtel(tel);
+			mvo.setMemail(mail);
+			
+			AdminDAO adao = new AdminDAO();
+			adao.reAdminNopass(mvo);
+
+		 }
+	 
+		try {
+			resp.sendRedirect("/YummyMap/admin/main.mmy");
+		} catch (Exception e) {}
+		String view = null;	
+		return view;
 	}
 
 }
