7119197dc32616625d5df614a66240aa9852c73a
aktmzk112 <aktmzk112@gmail.com>
2020-05-27 22:12:47 +0900

    commit

---
+++ YummyMap/src/com/yummymap/www/controller/board/ReplyRemveProc.java
@@ -0,0 +1,16 @@
+package com.yummymap.www.controller.board;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.yummymap.www.controller.MmyController;
+
+public class ReplyRemveProc implements MmyController {
+
+	@Override
+	public String exec(HttpServletRequest req, HttpServletResponse resp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
